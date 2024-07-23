f785b7d0c4e2c7ea8c46fd0a7b601fb6cbda5d9b
nathan harris <nharrisanalyst@gmail.com>
2024-07-22 22:05:57 -0700

    added test for RssFeed

--- rss/models.py
+++ rss/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from members.models import Members
+from django.utils.timezone import now
 
 # Create your models here.
 
 class RssURL(models.Model):
-    url = models.URLField(max_length=2000)
+    url = models.URLField(max_length=2000, unique=True)
     members = models.ManyToManyField(Members)
 
 
-class RssData(models.Model):
+class RssFeed(models.Model):
     url = models.ForeignKey(RssURL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    link = models.URLField(max_length=2000)
+    description = models.CharField(max_length=100)
+
+
+class RssPost(models.Model):
+    rss_feed = models.ForeignKey(RssFeed, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    link = models.URLField(max_length=2000)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False, default=now)
+    description = models.CharField(max_length=1000)
+
+
--- rss/tests.py
+++ rss/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from rss.models import RssURL
+from rss.models import (RssURL, 
+                        RssFeed)
 
 # Create your tests here.
 
@@ -11,4 +12,26 @@ class RssUrlTestCase(TestCase):
         RssURL.objects.create(url=self.test_url)
 
     def test_there_is_an_email(self):
-        RssURL.objects.get(url=self.test_url)
+        rss_url = RssURL.objects.get(url=self.test_url)
+        self.assertEqual(rss_url.url, self.test_url)
+
+
+class RssFeedTestCase(TestCase):
+    def setUp(self):
+        self.test_feed = {
+            'link':'www.example.com',
+            'title':'this is an example title',
+            'description':'This is an example description',
+        }
+        RssURL.objects.create(url=self.test_feed['link'])
+        self.rss_url = RssURL.objects.get(url=self.test_feed['link'])
+
+        RssFeed.objects.create(title = self.test_feed['title'], description= self.test_feed['description'], 
+                                                                link = self.test_feed['link'], url= self.rss_url)
+
+    def test_rss_feed_model(self):
+        rss_feed = RssFeed.objects.get(link=self.test_feed['link'])
+        self.assertEqual(rss_feed.link, self.test_feed['link'])
+
+        
+
