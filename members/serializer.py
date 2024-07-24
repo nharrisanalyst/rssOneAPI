187dc05a21cd470c17b5486bcde83312a3659a0e
nathan harris <nharrisanalyst@gmail.com>
2024-07-23 17:30:30 -0700

    added serializers to rss

--- members/serializer.py
+++ members/serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from members.models import Members
+from rss.serializers import RssURLSerializer
 
 class MembersSerializer(serializers.Serializer):
     email = serializers.CharField(allow_null = True)
     password = serializers.CharField(allow_null = True)
+    rssURLS = RssURLSerializer(many = True, read_only=True)
     
     def create(self, validated_data):
        return  Members.objects.create(**validated_data)
---
+++ rss/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from rss.models import (
+    RssURL
+)
+
+class RssPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        ordering =['-url']
+        model = RssURL
+        fields = ('url','title', 'link', 'description')
+
+class RssFeedSerializer(serializers.ModelSerializer):
+    posts = RssPostSerializer(many=True, read_only=True)
+    
+    class Meta:
+        ordering =['-url']
+        model = RssURL
+        fields = ('url','title', 'link', 'description')
+
+class RssURLSerializer(serializers.ModelSerializer):
+    feed = RssFeedSerializer(read_only=True)
+    class Meta:
+        ordering =['-url']
+        model = RssURL
+        fields = ('url','members', 'feed')
+
+
+
